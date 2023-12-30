047ef1736d6094162022696e78482a0171314b8e
nima <nimakalhor25@gmail.com>
2023-12-30 15:32:49 +0330

    bin bon

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Button>test button</Button>
+    </>
+  );
+}
+
+export default App;
